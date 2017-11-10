05cbcc898172f0990ce1f58ed17170cb5c99818f
Viacheslav Zhabonos <vyacheslav0310@gmail.com>
2017-11-10 12:22:36 +0200

    Added BeautifulSoap initialization

--- parser.py
+++ parser.py
@@ -4,6 +4,11 @@ from bs4 import BeautifulSoup
 class Parser:
     url = ''
     content = ''
+    soap = None;
+
+    def init_soap(self):
+        if not isinstance(soap, BeautifulSoup):
+            self.soap = BeautifulSoup(self.content, 'html.parser')
 
     def set_url(self, url):
         self.url = url
@@ -16,3 +21,7 @@ class Parser:
         if r.status_code == 200 and r.reason == 'OK':
             self.content = r.content;
         return self.content
+
+    def get_prettified_content(self):
+        soup = BeautifulSoup(self.content, 'html.parser')
+        return soup.prettify('UTF-8')
